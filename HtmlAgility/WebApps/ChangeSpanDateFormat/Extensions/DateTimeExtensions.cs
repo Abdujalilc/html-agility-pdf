95c93dcfbda5ecfadd62e862a579d4246d497237
abdujalilc <abdujalilc@outlook.com>
2024-02-29 13:16:33 +0500

    extension methods are added

---
+++ HtmlAgility/WebApps/ChangeSpanDateFormat/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using HtmlAgilityPack;
+using System.Xml;
+
+namespace ChangeSpanDateFormat.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToCustomDateString(this DateTime dateTime, string dateFormatTemplate)
+        {
+            // Convert the newValue to match the format of the template
+            string formattedDate = dateTime.ToString(dateFormatTemplate);
+            return formattedDate;
+        }
+    }
+}
