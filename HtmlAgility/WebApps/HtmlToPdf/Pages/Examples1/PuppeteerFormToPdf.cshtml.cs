9e0a16e9bb04da33146d7b0a0c7de26380e15493
abdujalilc <abdujalilc@gmail.com>
2024-09-19 17:13:51 +0500

    puppeteer form to pdf adding started

---
+++ HtmlAgility/WebApps/HtmlToPdf/Pages/Examples1/PuppeteerFormToPdf.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HtmlToPdfWeb.Pages.Examples1
+{
+    public class PuppeteerFormToPdfModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public void OnPost(string FullName, string Course, int Level)
+        {
+
+        }
+    }
+}
--- HtmlAgility/WebApps/HtmlToPdf/Pages/Examples1/PuppeteerHtmlToPdf.cshtml.cs
+++ HtmlAgility/WebApps/HtmlToPdf/Pages/Examples1/PuppeteerHtmlToPdf.cshtml.cs
@@ -8,9 +8,11 @@ namespace HtmlToPdfWeb.Pages.Examples1
     public class PuppeteerHtmlToPdfModel : PageModel
     {
         private readonly IWebHostEnvironment _env;
-        public PuppeteerHtmlToPdfModel(IWebHostEnvironment env)
+        private readonly IConfiguration _configuration;
+        public PuppeteerHtmlToPdfModel(IWebHostEnvironment env, IConfiguration configuration)
         {
             _env = env;
+            _configuration = configuration;
         }
         public void OnGet()
         {
@@ -20,17 +22,19 @@ namespace HtmlToPdfWeb.Pages.Examples1
         {
             try
             {
+                //string PathChrome = System.Configuration.ConfigurationManager.AppSettings.Get("PathChrome"); //for framwework
+                //string PathChrome = _configuration["PathChrome"] ?? "";
                 var launchOptions = new LaunchOptions
                 {
                     Headless = true,
                     ExecutablePath = @"c:\Program Files\Google\Chrome\Application\chrome.exe"
                 };
 
-                using (var browser = await Puppeteer.LaunchAsync(launchOptions))
-                using (var page = await browser.NewPageAsync())
+                using (IBrowser browser = await Puppeteer.LaunchAsync(launchOptions))
+                using (IPage page = await browser.NewPageAsync())
                 {
-                    var req = HttpContext.Request;
-                    var baseUrl = $"{req.Scheme}://{req.Host}{req.PathBase}";
+                    HttpRequest req = HttpContext.Request;
+                    string baseUrl = $"{req.Scheme}://{req.Host}{req.PathBase}";
                     string htmlPath = baseUrl + "/templates/01/index.html";
 
                     string pdfPath = Path.Combine(_env.WebRootPath, $"templates/pdfs/index.pdf");
@@ -38,7 +42,7 @@ namespace HtmlToPdfWeb.Pages.Examples1
                     await page.GoToAsync(htmlPath);
 
                     await page.WaitForSelectorAsync("link[href='styles.css']");
-                    await page.WaitForSelectorAsync(".wu-img-qr");                   
+                    await page.WaitForSelectorAsync(".wu-img-qr");
                     await page.WaitForSelectorAsync(".background-img");
 
                     {
